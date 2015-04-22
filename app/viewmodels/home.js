1410cb101080a1aec44c45cb8e1d44ddb645934a
hungv <hungvum@gmail.com>
2015-04-23 05:23:24 +0700

    intergrate friend suggestion
    intergrate friend suggestion

--- app/viewmodels/home.js
+++ app/viewmodels/home.js
@@ -1,8 +1,26 @@
-﻿define(function() {
-    var param = function () {
-        this.displayName = 'Not need at this time';
-        this.description = 'Not need at this time';
-    };
+﻿/**
+ * Created by dxhao on 4/22/2015.
+ */
 
-    return param;
+define(['plugins/http', 'durandal/app', 'knockout'], function (http, app, ko) {
+    return {
+        friends: ko.observableArray([]),
+
+        activate :function(){
+            var that = this;
+
+            $.getJSON("./localstorage/friends.json",function(datajson){
+                that.friends(datajson.friends.data);
+            });
+            return;
+        },
+        search: function(){
+            //return app.showDialog("Add friend not implement...");
+            return app.showMessage("Add friend not implement...");
+        },
+        canDeactivate: function () {
+            //the router's activator calls this function to see if it can leave the screen
+            return app.showMessage('Are you sure you want to leave this page?', 'Navigate', ['Yes', 'No']);
+        }
+    };
 });
