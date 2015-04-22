f219bba750809db1ddac8f4cf6813a6e6ccb1f01
TuongPham <tuongpv1992@gmail.com>
2015-04-22 17:45:52 +0700

    add sideBar_left to timeline

---
+++ app/viewmodels/sidebar_left.js
@@ -0,0 +1,13 @@
+define(['plugins/http', 'durandal/app', 'knockout'], function (http, app, ko){
+    var ctor = function () {
+        this.displayName = 'My Friend';
+
+    };
+
+    //Note: This module exports a function. That means that you, the developer, can create multiple instances.
+    //This pattern is also recognized by Durandal so that it can create instances on demand.
+    //If you wish to create a singleton, you should export an object instead of a function.
+    //See the "flickr" module for an example of object export.
+
+    return ctor;
+});
