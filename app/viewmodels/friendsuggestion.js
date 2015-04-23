245c6e2b5b77c78f728c6e710de43e836cd04e8d
hungv <hungvum@gmail.com>
2015-04-23 11:28:59 +0700

    edit js
    js

--- app/viewmodels/friendsuggestion.js
+++ app/viewmodels/friendsuggestion.js
@@ -32,9 +32,9 @@ define(['plugins/http', 'durandal/app', 'knockout'], function (http, app, ko) {
             //return app.showDialog("Add friend not implement...");
             return app.showMessage("Add friend not implement...");
         },
-        canDeactivate: function () {
+        /*canDeactivate: function () {
             //the router's activator calls this function to see if it can leave the screen
             return app.showMessage('Are you sure you want to leave this page?', 'Navigate', ['Yes', 'No']);
-        }
+        }*/
     };
 });
