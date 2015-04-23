5d6f4750351689d114e8306e40659421625f1733
dangduc411 <dangduc411@gmail.com>
2015-04-23 17:09:05 +0700

    binding data

--- app/viewmodels/group_suggestion.js
+++ app/viewmodels/group_suggestion.js
@@ -1,7 +1,36 @@
-define(['plugins/http', 'durandal/app', 'knockout'], function (http, app, ko){
+define(['plugins/http', 'durandal/app', 'knockout','jquery'], function (http, app, ko, $){
     return{
-        groupName: ko.observable("4html5"),
-        avartars: ko.observableArray(["./image/3.jpg","./image/2.jpg","./image/3.jpg","./image/2.jpg"]),
-        quantityMembers: ko.observable("12"+" Members")
+        groups: ko.observableArray([]),
+        name: ko.observable("tetssdas"),
+
+        activate :function(){
+
+          var that = this;
+
+          $.getJSON('app/dataModel/groups.json', function(datalocal){
+            var group_suggestion = [];
+
+            // random in range
+            function randomRange(min,max)
+            {
+              return Math.random() * (max-min) + min;
+            }
+
+            // get random a number in range of group suggestion in json
+            x = Math.round(randomRange(0, datalocal.groups.data.length-2));
+
+            // get the random obj and next of its
+            group_suggestion[0] = datalocal.groups.data[x];
+            group_suggestion[1] = datalocal.groups.data[x+1];
+            
+
+            // set group_suggestion = observableArray groups
+            that.groups(group_suggestion);
+
+          }).error(function(jqXhr, textStatus, error) {
+              alert("ERROR: " + textStatus + ", " + error);
+          });
+            this.name();
+        }
     }
 });
