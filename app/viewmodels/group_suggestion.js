1e7fe3e3e9bf56d84784b568b72d3e5454e1f28b
dangduc411 <dangduc411@gmail.com>
2015-04-23 10:18:23 +0700

    repare img

--- app/viewmodels/group_suggestion.js
+++ app/viewmodels/group_suggestion.js
@@ -1,7 +1,7 @@
 define(['plugins/http', 'durandal/app', 'knockout'], function (http, app, ko){
     return{
         groupName: ko.observable("4html5"),
-        listImages: ko.observableArray([]),
-        quantityMembers: ko.observable()
+        avartars: ko.observableArray(["./image/3.jpg","./image/2.jpg","./image/3.jpg","./image/2.jpg"]),
+        quantityMembers: ko.observable("12"+" Members")
     }
 });
