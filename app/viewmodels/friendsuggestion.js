c3c9f2bb4538b2aa4115bf226963e1a1e9b9dc4a
haodang11 <xuanhao0101@gmail.com>
2015-04-23 15:28:10 +0700

    Update Friend Suggestion

--- app/viewmodels/friendsuggestion.js
+++ app/viewmodels/friendsuggestion.js
@@ -1,16 +1,26 @@
 /**
  * Created by dxhao on 4/22/2015.
  */
-/**
- * Created by dxhao on 4/22/2015.
- */
 
 define(['plugins/http', 'durandal/app', 'knockout'], function (http, app, ko) {
     return {
-        name: ko.observable("Hào ??ng"),
-        sayHello: function() {
-            app.showMessage('Hello ' + this.name() + '! Nice to meet you.', 'Greetings');
+        friends: ko.observableArray([]),
+
+        activate :function(){
+            var that = this;
+
+            $.getJSON("./localstorage/friends.json",function(datajson){
+                that.friends(datajson.friends.data);
+            });
+            return;
+        },
+        search: function(){
+            //return app.showDialog("Add friend not implement...");
+            return app.showMessage("Add friend not implement...");
+        },
+        canDeactivate: function () {
+            //the router's activator calls this function to see if it can leave the screen
+            return app.showMessage('Are you sure you want to leave this page?', 'Navigate', ['Yes', 'No']);
         }
     };
 });
-
