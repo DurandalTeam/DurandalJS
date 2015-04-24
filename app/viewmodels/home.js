28ce55fa2de26f58d38082bd4991ae36ed08938b
haodang11 <xuanhao0101@gmail.com>
2015-04-24 08:36:23 +0700

    Update Get Random 3 Friend_ Friend suggestion

--- app/viewmodels/home.js
+++ app/viewmodels/home.js
@@ -1,8 +1,4 @@
-﻿/**
- * Created by dxhao on 4/22/2015.
- */
-
-define(['plugins/http', 'durandal/app', 'knockout'], function (http, app, ko) {
+﻿define(['plugins/http', 'durandal/app', 'knockout'], function (http, app, ko) {
     return {
         friends: ko.observableArray([]),
 
@@ -10,7 +6,21 @@ define(['plugins/http', 'durandal/app', 'knockout'], function (http, app, ko) {
             var that = this;
 
             $.getJSON("./localstorage/friends.json",function(datajson){
-                that.friends(datajson.friends.data);
+                var friends_suggestion=[];
+                function randomRange(min,max)
+                {
+                    return Math.random() * (max-min) + min;
+                }
+
+                // get random a number in range of group suggestion in json
+                x = Math.round(randomRange(0, datajson.friends.data.length-4));
+
+                // get the random obj and next of its
+                friends_suggestion[0] = datajson.friends.data[x];
+                friends_suggestion[1] = datajson.friends.data[x+1];
+                friends_suggestion[2] = datajson.friends.data[x+2];
+
+                that.friends(friends_suggestion);
             });
             return;
         },
