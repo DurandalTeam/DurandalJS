64bb9abf3777f4501e432671210aed2d4c2e4048
VanTranThe <ttvan20@gmail.com>
2015-04-27 09:16:26 +0700

    Update comment function of feed_ver2

--- app/viewmodels/commentfeed.js
+++ app/viewmodels/commentfeed.js
@@ -1,41 +1,34 @@
-define(['plugins/http', 'durandal/app', 'knockout'], function (http, app, ko) {
+define(['plugins/http', 'durandal/app', 'knockout', 'jquery', 'plugins/observable'], function (http, app, ko, $, observable) {
     //Note: This module exports an object.
     //That means that every module that "requires" it will get the same object instance.
     //If you wish to be able to create multiple instances, instead export a function.
     //See the "welcome" module for an example of function export.
 
     return {
-        listComment: ko.observableArray([]),
-        listCommentFilter: ko.observableArray([]),
+        LComment: ko.observableArray(),
+        listComment: [],
+        lengthCommentFilter: ko.observableArray([]),
         message: ko.observable(),
-        activate: function(){
+        activate: function() {
             var that = this;
-            $.getJSON("./jsonfile/commentfeed.json", function(cmt){
+            $.getJSON("app/dataModel/commentfeed.json", function (cmt) {
                 var temp = [];
-                //alert(cmt.video.data[0].created_time);
-                that.listComment(cmt.comments);
-                temp[0]=cmt.comments.data[0];
-                temp[1]=cmt.comments.data[1];
-                temp[2]=cmt.comments.data[2];
-                temp[3]=cmt.comments.data[3];
-                that.listCommentFilter = temp;
 
-            }).error(function(jqXhr, textStatus, error) {
+                that.lengthCommentFilter(cmt.comments.data.length - 4);
+                var i, j =0;
+                for (i = cmt.comments.data.length - 4; i <+ cmt.comments.data.length; i++) {
+                    temp[j] = cmt.comments.data[i];
+                    j++;
+                }
+                listComment = cmt.comments.data;
+                that.LComment(temp);
+            }).error(function (jqXhr, textStatus, error) {
                 alert("ERROR: " + textStatus + ", " + error);
             });
-
-            $(document).ready(function () {
-                $('.aaf').on("click",function(){
-                    var usersid =  $(this).attr("id");
-                    alert("click on link")
-                })
-            });
-
-            $('textarea').keypress(function(e){
-                    if (e.keyCode == 13) {
-                        alert("keypress enter");
-                    }
-                });
+        },
+        select: function() {
+            this.LComment(listComment);
+            $("#seemore").hide();
         }
     };
 });
