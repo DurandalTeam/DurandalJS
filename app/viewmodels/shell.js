46410784613ea84f12810c8049a6a23873e3be7d
TuongPham <tuongpv1992@gmail.com>
2015-04-27 15:39:38 +0700

    update menu  top

--- app/viewmodels/shell.js
+++ app/viewmodels/shell.js
@@ -1,14 +1,31 @@
 ﻿define(['plugins/router', 'durandal/app'], function (router, app) {
     return {
         router: router,
+
+
         search: function() {
             //It's really easy to show a message box.
             //You can add custom options too. Also, it returns a promise for the user's response.
             app.showMessage('Search not yet implemented...');
         },
         activate: function () {
+
+            var that = this;
+           $.getJSON("app/dataModel/information.json", function(infData){
+
+                that.photo = infData.photo ;
+                that.name = infData.name ;
+
+            }).error(function(jqXhr, textStatus, error) {
+                alert("ERROR: " + textStatus + ", " + error);
+            });
+
+
+
+
+
             router.map([
-                { route: 'timeline',  moduleId: 'viewmodels/timeline', nav: true },
+                { route: 'timeline',  moduleId: 'viewmodels/timeline', nav: false },
                 { route: '',title:'Home', moduleId: 'viewmodels/home', nav: true },
                 //{ route: 'friend_suggestion', moduleId: 'viewmodels/friendsuggestion', nav: true },
                 //{ route: 'sideBar_left_home', moduleId: 'viewmodels/sideBar_left_home', nav: true },
