e52daf50d6a12d62ef86520f738e3bdbfebfc5a1
haodang11 <xuanhao0101@gmail.com>
2015-05-04 17:53:28 +0700

    friendsuggestion update

--- app/viewmodels/friendsuggestion.js
+++ app/viewmodels/friendsuggestion.js
@@ -11,14 +11,9 @@ define(['plugins/http', 'durandal/app', 'knockout'], function (http, app, ko) {
 
             $.getJSON("./localstorage/friends.json",function(datajson){
                 var friends_suggestion=[];
-                function randomRange(min,max)
-                {
-                    return Math.random() * (max-min) + min;
-                }
 
-                // get random a number in range of group suggestion in json
-                x = Math.round(randomRange(0, datajson.friends.data.length-4));
-
+                //return Math.random() * (max-min) + min;
+                x = Math.round(Math.random() * (datajson.friends.data.length-4));
                 // get the random obj and next of its
                 friends_suggestion[0] = datajson.friends.data[x];
                 friends_suggestion[1] = datajson.friends.data[x+1];
@@ -31,7 +26,7 @@ define(['plugins/http', 'durandal/app', 'knockout'], function (http, app, ko) {
         search: function(){
             //return app.showDialog("Add friend not implement...");
             return app.showMessage("Add friend not implement...");
-        },
+        }
         /*canDeactivate: function () {
             //the router's activator calls this function to see if it can leave the screen
             return app.showMessage('Are you sure you want to leave this page?', 'Navigate', ['Yes', 'No']);
